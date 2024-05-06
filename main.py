91b0be10c429bcf4b412eb0d7dfc487d50f7e4a9
Kim Chanho <beroso20th@gmail.com>
2024-05-06 12:40:33 +0900

    curd완성
    curd완성

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+
+class Memo(BaseModel):
+    id:int
+    content:str
+
+memos = []
+
+app = FastAPI()
+
+@app.post('/memos')
+def create_memo(memo:Memo):
+    memos.append(memo) 
+    return "메모 추가에 성공했습니다."
+
+@app.get('/memos')
+def read_memo():
+    return memos
+
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo):
+    for memo in memos: 
+        if memo.id == req_memo.id:
+            memo.content = req_memo.content
+            return '성공했습니다.'
+    return '그런 메모는 없습니다'            
+
+@app.delete("/memos/{memo_id}")    
+def delete_memo(memo_id):
+    for index, memo in enumerate(memos): 
+        if str(memo.id) == str(memo_id):
+            memos.pop(index)
+            return('삭제성공')
+
+    return('삭제실패')
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+
+
+
+
+
+
---
+++ static/app.js
@@ -0,0 +1,81 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  const editInput = prompt("수정할 값을 입력하세요");
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+      content: editInput,
+    }),
+  });
+  readMemo();
+}
+
+async function deleteMemo(event) {
+  const id = event.target.dataset.id;
+  const res = await fetch(`/memos/${id}`, {
+    method: "DELETE",
+  });
+
+  readMemo();
+}
+
+function displayMemo(memo) {
+  const ul = document.querySelector("#memo-ul");
+  const li = document.createElement("li");
+  li.innerText = `${memo.content}`;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id;
+
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "삭제";
+  delBtn.addEventListener("click", deleteMemo);
+  delBtn.dataset.id = memo.id;
+
+  ul.appendChild(li);
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
+}
+
+async function readMemo() {
+  const res = await fetch("/memos");
+  const jsonRes = await res.json();
+  console.log(jsonRes);
+  const ul = document.querySelector("#memo-ul");
+  ul.innerHTML = "";
+  jsonRes.forEach(displayMemo);
+  //   displayMemo(jsonRes[jsonRes.length - 1]);
+}
+
+// await를 function에서 쓸 때는 async를 붙여줘야 함
+async function createMemo(value) {
+  const res = await fetch("/memos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: new Date().getTime(),
+      content: value,
+    }),
+  });
+  readMemo();
+}
+
+function handleSubmit() {
+  event.preventDefault();
+  const input = document.querySelector("#memo-input");
+  createMemo(input.value);
+  input.value = "";
+}
+
+const form = document.querySelector("#memo-form");
+form.addEventListener("submit", handleSubmit);
+
+readMemo();
