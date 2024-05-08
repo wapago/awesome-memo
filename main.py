8f22922beda9c3689653e1ca6ae9f2bdd4621fa3
Kim Chanho <beroso20th@gmail.com>
2024-05-08 22:57:11 +0900

    MongoDB 활용 memo수정 구현

--- main.py
+++ main.py
@@ -13,67 +13,49 @@ class Memo(BaseModel):
     id:int
     content:str
 
-memos = []
+def get_memo_from_mongodb():
+    memos_in_db = list(collection.find({}, {"_id": 0})) # mongodb atlas에 저장된 객체고유id는 제외하고 반환
+    return memos_in_db
 
 app = FastAPI()
 
 @app.post('/memos')
 def create_memo(memo:Memo):
-    print(memo)
-    
     data_to_insert = dict(memo)
-    insert_result = collection.insert_one(data_to_insert)
+    collection.insert_one(data_to_insert)
 
-    # 삽입 결과 확인
-    print("====== Inserted document ID ======")
-    print(insert_result.inserted_id)
-    memos.append(memo) 
     return "메모 추가에 성공했습니다."
 
 
 @app.get('/memos')
 def read_memo(order : Optional[str] = None, createAt: Optional[str] = None):
-    
-    if(order == 'asc') :
-        sorted_memos = sorted(memos, key=lambda x: x.content)
-
-        if(createAt == 'asc'):
-            sorted_memos = sorted(sorted_memos, key=lambda x: x.id)
-        elif(createAt == 'desc'):
-            sorted_memos = sorted(sorted_memos, key=lambda x: x.id, reverse=True)
-
-        return sorted_memos
-    elif(order == 'desc') :
-        sorted_memos = sorted(memos, key=lambda x: x.content, reverse=True)
-
-        if(createAt == 'asc'):
-            sorted_memos = sorted(sorted_memos, key=lambda x: x.id)
-        elif(createAt == 'desc'):
-            sorted_memos = sorted(sorted_memos, key=lambda x: x.id, reverse=True)
-            
-        return sorted_memos
-        
-    return memos
+    memos_in_db = get_memo_from_mongodb()
+    return memos_in_db
 
 @app.put("/memos/{memo_id}")
 def put_memo(req_memo:Memo):
-    print(req_memo.id)
-    for memo in memos: 
-        print(memo.id)
-        if memo.id == req_memo.id:
-            memo.content = req_memo.content
+    memos_in_db =get_memo_from_mongodb()
+    for memo in memos_in_db:
+        if memo['id'] == req_memo.id:
+            collection.update_one(
+                memo,
+                {"$set": {"content": req_memo.content}}
+            )
             return '성공했습니다.'
+        
+    read_memo()   
+
     return '그런 메모는 없습니다'            
 
-# 2. "return('삭제실패')"가 for반복문 밖에 위치하는 경우
+
 @app.delete("/memos/{memo_id}")    
 def delete_memo(memo_id):
-    # memos = [] 안의 memo를 하나씩 비교할 때
-    for index, memo in enumerate(memos): 
+    memos_in_db = get_memo_from_mongodb()
+    for index, memo in enumerate(memos_in_db): 
         if str(memo.id) == str(memo_id):
-            memos.pop(index)
+            memos_in_db.pop(index)
             return('삭제성공')
-    # for문 밖에 있으므로 모든 memos를 비교한 후에 같은 것이 없을 때 '삭제실패'를 return함
+        
     return('삭제실패')
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
